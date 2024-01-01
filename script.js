b215855622fd7808856d220a376025472d0acdce
Himumi <erliansyaha@gmail.com>
2024-01-01 21:23:47 +0900

    Set a hover effect

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ const container = document.querySelector('#container');
 function grid(e) {
     for (let i = 0; i < e; i++) {
         const row = document.createElement('div');
-        row.classList.add('row', 'row-' + i);
+        row.classList.add('row');
         container.appendChild(row);
         
         for (let i = 0; i < e; i++) {
             const item = document.createElement('div');
-            item.classList.add('item', 'item-' + i);
+            item.classList.add('item');
             row.appendChild(item);
         }
     }
+
+    const divs = document.querySelectorAll('.item');
+    divs.forEach(div => {
+        div.addEventListener('mouseover', () =>{
+            div.classList.add('black');
+        }); 
+    });
 }
 
-grid(16);
+grid(32);
