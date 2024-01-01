5166ec7d447b9fda8c5ffc242196aaba44336790
Himumi <erliansyaha@gmail.com>
2024-01-01 22:49:25 +0900

    Add button grid to ask how many the grids

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.querySelector('#container');
 function grid(e) {
     for (let i = 0; i < e; i++) {
         const row = document.createElement('div');
-        row.classList.add('row');
+        row.classList.add('row', i);
         container.appendChild(row);
         
         for (let i = 0; i < e; i++) {
@@ -21,4 +21,19 @@ function grid(e) {
     });
 }
 
-grid(32);
+grid(16);
+
+const gridButton = document.querySelector('#grid');
+
+gridButton.addEventListener('click', () => {
+    let input = +prompt("Please input number for grid(1 - 100).");
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(row => {
+        container.removeChild(row);
+    });
+    grid(input);
+    const divs = document.querySelectorAll('.item');
+    divs.forEach(div => {
+        div.classList.remove('black');
+    });
+});
